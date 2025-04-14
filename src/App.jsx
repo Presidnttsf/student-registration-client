6003c25058f88ba750692975960123855ee358e7
YourUsername <presidnttsf@gmail.com>
2025-04-14 11:12:40 +0530

    final version 1 updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
-import './App.css';
-import StudentList from './components/StudentList';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import { useState } from 'react';
+import PrivateRoute from './components/PrivateRoute';
+import StudentList from './components/StudentList';
+import Login from './components/Login';
+import Signup from './components/SignUp';  // Import Signup component
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const username = "Tauseef";
+  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('authToken'));
+  
+
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('username'); // Remove the username from localStorage as well
+    setIsLoggedIn(false);
+    
+  };
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+     // Set the username from the API response
+  };
 
   return (
-    <>
-      <Layout
-        isLoggedIn={isLoggedIn}
-        username={username}
-        onLogin={() => setIsLoggedIn(true)}
-        onLogout={() => setIsLoggedIn(false)}
-      >
-     
-        <StudentList /> {/* Now StudentList is part of the Layout component */}
-      </Layout>
-    </>
+    <Router>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        <Route path="/" element={<Signup />} /> {/* Signup Route landing page */}
+
+        {/* Protected Routes */}
+        <Route element={<PrivateRoute />}>
+          <Route element={<Layout isLoggedIn={isLoggedIn}  onLogout={handleLogout} />}>
+            <Route path="/students" element={<StudentList />} />
+          </Route>
+        </Route>
+
+        {/* Default Route */}
+      </Routes>
+    </Router>
   );
 }
 
